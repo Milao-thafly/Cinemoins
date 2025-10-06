3889c1ab12708bfbbbc4e24d89ded7d4c2db9da8
Martin Boizard <martinboizard@yahoo.fr>
2025-10-06 07:56:58 +0200

    Carousel updated to the three in the website

--- cinemoins/src/ui/CardList/CardList.tsx
+++ cinemoins/src/ui/CardList/CardList.tsx
@@ -8,27 +8,48 @@ import  { useCarousel } from "../../core/slide/useCarroussel"
 export const CardList = () => {
     const popularMovies = useFetcher<ApiListResult<Movie[]>>("movie/popular")
     const legitVoteMovies = useFetcher<ApiListResult<Movie[]>>("movie/top_rated")
-    const upcommingMovies = useFetcher<ApiListResult<Movie[]>>("movie/upcoming")
+    const upcomingMovies = useFetcher<ApiListResult<Movie[]>>("movie/upcoming")
 
-    const { start, end, next, prev } = useCarousel(popularMovies.movies || [], 6);
+    // const { start, end, next, prev } = useCarousel(popularMovies.movies || [], 6);
     // const { start, end, next, prev } = useCarousel(legitVoteMovies.movies || [], 6);
 
-        
+       const {
+    start: popularStart,
+    end: popularEnd,
+    next: nextPopular,
+    prev: prevPopular,
+  } = useCarousel(popularMovies.movies || [], 6);
+
+  const {
+    start: legitStart,
+    end: legitEnd,
+    next: nextLegit,
+    prev: prevLegit,
+  } = useCarousel(legitVoteMovies.movies || [], 6);
+
+  const {
+    start: upcomingStart,
+    end: upcomingEnd,
+    next: nextUpcoming,
+    prev: prevUpcoming,
+  } = useCarousel(upcomingMovies.movies || [], 6);
+  
+  
 
     if (popularMovies.isLoading) return <p>Charge maintenant...</p>
-
     if (!popularMovies.isLoading && !popularMovies.isError) {
     // console.log(popularMovies.movies.map(m => m.poster_path));  
-}
-    if (popularMovies.isError) return <p>Retourne chez toi</p>
+}if (popularMovies.isError) return <p>Retourne chez toi</p>
 
-        if (legitVoteMovies.isLoading) return <p>Charge maintenant...</p>
-
+    if (legitVoteMovies.isLoading) return <p>Charge maintenant...</p>
     if (!legitVoteMovies.isLoading && !legitVoteMovies.isError) {
     // console.log(popularMovies.movies.map(m => m.poster_path));  
-}
-    if (legitVoteMovies.isError) return <p>Retourne chez toi</p>
+}if (legitVoteMovies.isError) return <p>Retourne chez toi</p>
 
+    if (upcomingMovies.isLoading) return <p>Charge maintenant...</p>
+    if (!upcomingMovies.isLoading && !upcomingMovies.isError) {
+    // console.log(popularMovies.movies.map(m => m.poster_path));  
+}   if (upcomingMovies.isError) return <p>Retourne chez toi</p>
 
 return (
 <>
@@ -37,12 +58,13 @@ return (
 
   <div className="arrowed-carrousel">
             <div className="container-arrow">
-              <button className="arrow-left" onClick={prev}> {"<"} </button>
+              <button className="arrow-left" onClick={prevPopular}> {"<"} </button>
 
             </div>
           <ul className="caroussel">
 
-            {popularMovies.movies.slice(start, end ).map((movie) => {
+            {popularMovies.movies.slice(popularStart, popularEnd).map((movie) => {
+    // const { start, end, next, prev } = useCarousel(popularMovies.movies || [], 6);
 
               return (
                 <li key={movie.id}>
@@ -60,23 +82,24 @@ return (
             
           </ul>
             <div className="container-arrow">
-              <button className="arrow-right" onClick={next}> {">"} </button>
+              <button className="arrow-right" onClick={nextPopular}> {">"} </button>
               
             </div>
   </div>
 
 
-<h2>Top Movies</h2>
+<h2>Best Rate</h2>
   <div className="arrowed-carrousel">
 
             <div className="container-arrow">
-              <button className="arrow-left" onClick={prev}> {"<"} </button>
+              <button className="arrow-left" onClick={prevLegit}> {"<"} </button>
 
             </div>
         <ul className="caroussel">
           
-          {legitVoteMovies.movies.map((movie) => {
-            
+          {legitVoteMovies.movies.slice(legitStart, legitEnd).map((movie) => {
+                // const { start, end, next, prev } = useCarousel(legitVoteMovies.movies || [], 6);
+
             console.log(movie.poster_path); 
             return (
               <li key={movie.id}>
@@ -92,7 +115,7 @@ return (
           
         </ul>
         <div className="container-arrow">
-              <button className="arrow-right" onClick={next}> {">"} </button>
+              <button className="arrow-right" onClick={nextLegit}> {">"} </button>
               
             </div>
   </div>
@@ -102,13 +125,15 @@ return (
 <h2>Upcoming Movies</h2>
   <div className="arrowed-carrousel">
     <div className="container-arrow">
-              <button className="arrow-left" onClick={prev}> {"<"} </button>
+              <button className="arrow-left" onClick={prevUpcoming}> {"<"} </button>
 
             </div>
           <ul className="caroussel">
 
               
-            {upcommingMovies.movies.map((movie) => {
+            {upcomingMovies.movies.slice(upcomingStart, upcomingEnd).map((movie) => {
+                  // const { start, end, next, prev } = useCarousel(upcommingMovies.movies || [], 6);
+
               console.log(movie.poster_path); 
               return (
                 <li key={movie.id}>
@@ -125,7 +150,7 @@ return (
           </ul>
 
             <div className="container-arrow">
-              <button className="arrow-right" onClick={next}> {">"} </button>
+              <button className="arrow-right" onClick={nextUpcoming}> {">"} </button>
               
             </div>
   </div>
